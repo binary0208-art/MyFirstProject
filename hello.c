dfb92419e33147c83fc280ec46b207a93d3296b8
binary0208 <binary0208@gmail.com>
2021-03-15 01:19:43 +0800

    Added a new hello world c program

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-15 01:14:25
+ * @LastEditTime: 2021-03-15 01:14:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Hello\hello.c
+ */
+int main()
+{
+
+    printf("hello world\n");
+
+    return 0;
+}
